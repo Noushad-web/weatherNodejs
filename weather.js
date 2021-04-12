351596d26b18f9cc59ce5bfe4b0752321ca4c9a5
Mohammad Noushad Siddiqi <noushadsheikhpb@gmail.com>
2021-04-12 10:09:16 +0530

    add the designs

--- weather.js
+++ weather.js
@@ -1,5 +1,7 @@
 const http = require('http');
+
 const fs = require('fs');
+
 const requests = require('requests');
 // const {}
 
@@ -8,30 +10,29 @@ const weatherhtml = fs.readFileSync('weather.html', 'utf-8');
 const replaceVal = (weatherhtml, apiVal) => {
     // new html file 
     let tempFar = apiVal.main.temp;
-    let tempCal = (tempFar - 32) * 5 / 9;
-    let updatedWeatherHtml = weatherhtml.replace('{%tempvalue%}', tempCal);
-    fs.writeFile('weather.html', updatedWeatherHtml, (err) => {
-        // console.log(err);
-    })
+    // let tempCal = tempFar - 23;
+    let updatedWeatherHtml = weatherhtml.replace('{%tempvalue%}', tempFar);
+    return updatedWeatherHtml;
 }
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
         requests(
-            "http://api.openweathermap.org/data/2.5/weather?q=Antarctica&appid=3f2fc6590fd49a886500d3f35c92fb26",
+            "http://api.openweathermap.org/data/2.5/weather?q=Ludhiana&appid=3f2fc6590fd49a886500d3f35c92fb26",
         )
             .on('data', function (chunk) {
                 let weatherdataObj = JSON.parse(chunk);
                 let arrData = [weatherdataObj];
-                console.log(chunk);
-                let newArrData = arrData.map((val) => {
-                    replaceVal(weatherhtml, val);
-                }) 
+                console.log(arrData);
+                let mappedArr = arrData.map((val) => {
+                    replaceVal(weatherhtml, val).join("");
+                });
             
             })
             .on('end', function (err) {
                 if (err) console.log('connection closed due to erros', err);            
                 console.log('end');
+                res.end();
             });
     } else {
         console.log('page not found');
