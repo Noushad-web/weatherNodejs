a64b03158752b88da928b5ecdda51e7a84d30e4a
aniketbhalla2544 <aniketbhalla2544@gmail.com>
2021-04-12 00:25:18 -0400

    weather app html running

--- weather.js
+++ weather.js
@@ -6,13 +6,15 @@ const requests = require('requests');
 const weatherhtml = fs.readFileSync('weather.html', 'utf-8');
 
 const replaceVal = (weatherhtml, apiVal) => {
-    // new html file 
-    let tempFar = apiVal.main.temp;
-    let tempCal = (tempFar - 32) * 5 / 9;
+
+    // accessing temperature from api data
+    const { main: { temp: tempFar, temp_min: minTemp, temp_max: maxTemp }, name: location } = apiVal;
+    const tempCal = (tempFar - 32) * 5 / 9;
+
     let updatedWeatherHtml = weatherhtml.replace('{%tempvalue%}', tempCal);
-    fs.writeFile('weather.html', updatedWeatherHtml, (err) => {
-        // console.log(err);
-    })
+    // updatedWeatherHtml = updatedWeatherHtml.replace('');
+    return updatedWeatherHtml;
+
 }
 
 const server = http.createServer((req, res) => {
@@ -20,18 +22,21 @@ const server = http.createServer((req, res) => {
         requests(
             "http://api.openweathermap.org/data/2.5/weather?q=Antarctica&appid=3f2fc6590fd49a886500d3f35c92fb26",
         )
-            .on('data', function (chunk) {
-                let weatherdataObj = JSON.parse(chunk);
-                let arrData = [weatherdataObj];
-                console.log(chunk);
-                let newArrData = arrData.map((val) => {
-                    replaceVal(weatherhtml, val);
-                }) 
-            
+
+            // when data is available
+            .on('data', chunk => {
+
+                const jsonDataObj = JSON.parse(chunk);
+                const arrJsonData = [jsonDataObj];
+
+                let realTimeData = arrJsonData.map(dataObj => replaceVal(weatherhtml, dataObj));
+                realTimeData = realTimeData.join("");
+                // console.log(realTimeData);
+                res.write(realTimeData);
             })
-            .on('end', function (err) {
-                if (err) console.log('connection closed due to erros', err);            
-                console.log('end');
+            .on('end', err => {
+                if (err) console.log('connection closed due to erros', err);
+                res.end();
             });
     } else {
         console.log('page not found');
@@ -39,7 +44,7 @@ const server = http.createServer((req, res) => {
 
 });
 
-server.listen('4100' , '127.0.0.1' , () => {
+server.listen('4100', '127.0.0.1', () => {
     console.log('We are not dumb');
 })
 
